7532d03e92d1a9b68df7fa5f0e72a610b02054a2
Danson Muia <danson.mwendwa@gmail.com>
2023-12-04 16:04:50 +0300

    Allow cors

--- main.py
+++ main.py
@@ -1,11 +1,22 @@
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 
 from app import auth, users, customers, messages
 
 # Import Base here for discovery by alembic
 from app.database import Base
 
+allowed_origins = ["http://localhost:3000", "http://127.0.0.1:3000"]
+
 app = FastAPI()
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=allowed_origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"]
+)
 
 app.include_router(auth.router, tags=["Auth"])
 app.include_router(users.router, prefix="/users", tags=["Users"])
